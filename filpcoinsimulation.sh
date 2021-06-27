65157b14f9deaadfd7cd825d1c9cc57e5e4fce7a
lavanya-pg <lavanyapg8@gmail.com>
2021-06-27 10:59:51 +0530

    ADDED:filping a coin multiple times

--- filpcoinsimulation.sh
+++ filpcoinsimulation.sh
@@ -1,9 +1,19 @@
 #!/bin/bash -x
 echo "Welcome to Filpcoin simulation"
-random=$((RANDOM%2))
-if [ $random -eq 1 ]
-then
-	echo "heads"
-else
-	echo "tails"
-fi
+read -p "how much times you want to toss the coin: " num
+headcount=0
+tailcount=0
+for((i=1;i<=$num;i++))
+do
+
+	random=$((RANDOM%2))
+	if [ $random -eq 1 ]
+	then
+		((headcount++))
+	else
+		((tailcount++))
+	fi
+done
+
+echo "head won $headcount times"
+echo "tail won $tailcount times"
